54724b649781b5c1e1252c1b0c20b9cb122e52c7
dawidorpel <143597333+dawidorpel@users.noreply.github.com>
2024-02-29 10:18:26 +0100

    123

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ var swiper = new Swiper(".home-slider",{
     spaceBetween: 30,
     centeredSliders: true,
     autoplay: {
-        delay: 7500,
+        delay: 4500,
         disableOnInteraction: false,
     },
 pagination: {
