d05215cd7190147244a54614f17150eeae705fb7
dawidorpel <143597333+dawidorpel@users.noreply.github.com>
2024-03-13 18:23:12 +0100

    loader.exe

--- script.js
+++ script.js
@@ -6,9 +6,29 @@ menu.onclick = () => {
     navbar.classList.toggle('active');
 }
 
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
+
 window.onscroll = () => {
+
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
+    
+    section.forEach(sec => {
+
+        let top = window.scrollY;
+        let height = sec.offHeight;
+        let offset = sec.offsetTop - 150;
+        let id = sec.getAttribute('id');
+
+        if(top => offset && top < offset + height){
+            navLinks.forEach(links =>{
+                links.classList.remove('active');
+                document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+            });
+        };
+    });
+
 }
 
 document.querySelector('#search-icon').onclick = () => {
@@ -61,3 +81,12 @@ breakpoints:
     },
 },
 });
+
+
+function loader(){
+    document.querySelector('.loader-cotainer').classList.add('fade-out');
+}
+function fadeOut(){
+    setInterval(loader, 2000)
+}
+window.onload=fadeOut;
